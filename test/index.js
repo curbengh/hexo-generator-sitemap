954df6d41d86abfc533bbb676cf19032830f7b92
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-23 10:05:14 +0930

    test: use hexo-generator-feed approach

--- test/index.js
+++ test/index.js
@@ -2,8 +2,30 @@
 
 const should = require('chai').should(); // eslint-disable-line
 const Hexo = require('hexo');
+const nunjucks = require('nunjucks');
+const env = new nunjucks.Environment();
+const pathFn = require('path');
+const fs = require('fs');
+const { format, parse } = require('url');
 const cheerio = require('cheerio');
-const urlFn = require('url');
+
+env.addFilter('uriencode', str => {
+  if (str) {
+    return format({
+      protocol: parse(str).protocol,
+      hostname: parse(str).hostname,
+      pathname: encodeURI(parse(str).pathname)
+    });
+  }
+});
+
+const sitemapTmplSrc = pathFn.join(__dirname, '../sitemap.xml');
+const sitemapTmpl = nunjucks.compile(fs.readFileSync(sitemapTmplSrc, 'utf8'), env);
+
+const urlConfig = {
+  url: 'http://localhost/',
+  root: '/'
+};
 
 describe('Sitemap generator', () => {
   const hexo = new Hexo(__dirname, {silent: true});
@@ -12,26 +34,26 @@ describe('Sitemap generator', () => {
   };
   const Post = hexo.model('Post');
   const generator = require('../lib/generator').bind(hexo);
-  const sitemapTmpl = require('../lib/template')(hexo.config);
+
+  require('../node_modules/hexo/lib/plugins/helper')(hexo);
+
   let posts = {};
   let locals = {};
 
   before(() => {
-    return hexo.init().then(() => {
-      return Post.insert([
-        {source: 'foo', slug: 'foo', updated: 1e8},
-        {source: 'bar', slug: 'bar', updated: 1e8 + 1},
-        {source: 'baz', slug: 'baz', updated: 1e8 - 1}
-      ]).then(data => {
-        posts = Post.sort('-updated');
-        locals = hexo.locals.toObject();
-      });
+    return Post.insert([
+      {source: 'foo', slug: 'foo', updated: 1e8},
+      {source: 'bar', slug: 'bar', updated: 1e8 + 1},
+      {source: 'baz', slug: 'baz', updated: 1e8 - 1}
+    ]).then(data => {
+      posts = Post.sort('-updated');
+      locals = hexo.locals.toObject();
     });
   });
 
   it('default', () => {
+    hexo.config = Object.assign(hexo.config, urlConfig);
     const result = generator(locals);
-
     result.path.should.eql('sitemap.xml');
     result.data.should.eql(sitemapTmpl.render({
       config: hexo.config,
@@ -70,18 +92,27 @@ describe('Sitemap generator', () => {
   });
 
   it('IDN handling', () => {
-    hexo.config.url = 'http://fôo.com/bár';
-    const parsedUrl = urlFn.format({
-      protocol: urlFn.parse(hexo.config.url).protocol,
-      hostname: urlFn.parse(hexo.config.url).hostname,
-      pathname: encodeURI(urlFn.parse(hexo.config.url).pathname)
-    });
+    const checkURL = function(url, root) {
+      hexo.config.url = url;
+      hexo.config.root = root;
 
-    const result = generator(locals);
-    const $ = cheerio.load(result.data);
+      const result = generator(locals);
+      const $ = cheerio.load(result.data);
 
-    $('url').each((index, element) => {
-      $(element).children('loc').text().startsWith(parsedUrl).should.be.true;
-    });
+      const punyIDN = format({
+        protocol: parse(url).protocol,
+        hostname: parse(url).hostname,
+        pathname: encodeURI(parse(url).pathname)
+      });
+
+      $('url').each((index, element) => {
+        console.log($(element).children('loc').text())
+        $(element).children('loc').text().startsWith(punyIDN).should.be.true;
+      });
+    };
+
+    checkURL('http://fôo.com/', '/');
+
+    checkURL('http://fôo.com/bár', '/bár/');
   });
 });
